5c103fde5ce721578dfa25675e08b57000dd6897
Balint-H <bkh16@ic.ac.uk>
2024-04-17 13:06:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SSNR 2024 WS5 material
+
+Clone the repository (if you have Git) or download the files directly by pressing the green button labelled `<> Code ▾`. Follow the instructions inside the .pdf file to set up a python environment ready to run the files and challenges.
+
+Currently houses content mostly from the 2023 version, the new material for this year will be added closer to the summer school date.
---
+++ welcome_scene/hello_ssnr.py
@@ -0,0 +1,16 @@
+# This script just loads a MuJoCo xml file without performing any logic in python.
+# Alternatively you could just drag and drop an xml file after running "simulate.exe"
+# from the MuJoCo directory downloaded.
+
+import mujoco
+import mujoco.viewer as viewer
+import matplotlib
+matplotlib.use("QtAgg")
+
+# Change this string to other scenes you may want to load. You can also open the xml in a code editor
+# to examine its contents. For more instructions check out the header comments of xml/01_planar_arm.xml
+xml = r'welcome_scene/welcome.xml'
+
+
+if __name__ == '__main__':
+    viewer.launch(loader=viewer._file_loader(xml), show_left_ui=False, show_right_ui=False)
